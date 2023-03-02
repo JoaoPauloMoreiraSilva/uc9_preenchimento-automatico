d7633af76daa73a1155cb7a655a9114d8ea5c764
JOAO PAULO MOREIRA SILVA <joao.pmsilva47@senacsp.edu.br>
2023-03-01 21:53:40 -0300

    Joao

---
+++ uc9_preenchimento automatico/Program.cs
@@ -0,0 +1,30 @@
+﻿string[,] matriz = new string[9, 9];
+int l, c;
+
+for  (l = 0; l < 9; l++)
+{
+    for (c = 0; c < 9; c++)
+    {
+        if (l == c)
+        {
+            matriz[l, c] = "X";
+        }
+        else
+        {
+            matriz[l, c] = "0";
+        }
+    }
+}
+
+for (l = 0; l < 9; l++)
+{
+    for (c = 0; c < 9; c++)
+    {
+        Console.Write(matriz[l, c] + " ");
+    }
+    Console.WriteLine("");
+}
+
+
+
+
